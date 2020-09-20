a9b3543c55ab2a8007d518014cb4a0428950a7e8
Mitali Kaushal <mitali.kaushal9@gmail.com>
2020-09-19 20:20:18 -0700

    created react app, added sass and created homepage component

---
+++ clothing_app/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import HomePage from './homepage';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <HomePage/>
+      </div>
+    );
+  }
+}
+
+export default App;
