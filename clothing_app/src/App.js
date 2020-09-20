f2d63117110815056007cfa359fa0516473b4c5a
Mitali Kaushal <mitali.kaushal9@gmail.com>
2020-09-20 15:49:57 -0700

    Adding react router functionality

--- clothing_app/src/App.js
+++ clothing_app/src/App.js
@@ -1,16 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter ,Switch, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <HomePage/>
-      </div>
-    );
-  }
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE </h1>
+  </div>
+);
+
+function App() {
+  return (
+    <div>
+    <BrowserRouter>
+    <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/hats' component={HatsPage} />
+      </Switch>
+    </BrowserRouter>
+
+    </div>
+  );
 }
 
 export default App;
